fb61b464d813cd8dcfd7a136874f7b2fd8e500ec
Voi <wojtek.wacek.dola@gmail.com>
2023-04-26 14:35:58 +1000

    Changed functions to async await for get and delete.

--- routes/notes.js
+++ routes/notes.js
@@ -1,25 +1,36 @@
 const notes = require("express").Router();
+const fs = require("fs");
 
 // GET Route for retrieving the notes
-notes.get("/", (req, res) => {
-    readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+notes.get("/", async (req, res) => {
+  let data = await fs.promises.readFile("./db/db.json", "utf-8");
+  let parsedData = await res.json(JSON.parse(data));
+  return parsedData;
+  //   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
 // DELETE Route for a specific note
-notes.delete("/:id", (req, res) => {
-    const noteId = req.params.id;
-readFromFile("./db/db.json")
-  .then((data) => JSON.parse(data))
-  .then((json) => {
-    // Make a new array of all notes except the one with the ID provided in the URL
-    const result = json.filter((note) => note.id !== noteId);
+notes.delete("/:id", async (req, res) => {
+  const noteId = req.params.id;
+  let data = await fs.promises.readFile("./db/db.json", "utf-8");
+  let parsedData = await res.json(JSON.parse(data));
+  let result = await parsedData.filter((note) => note.id !== noteId);
+  fs.writeFile("./db/db.json", JSON.stringify(result, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to db.json`)
+  );
+  res.json(`Item ${noteId} has been deleted.`);
+  //   readFromFile("./db/db.json")
+  //     .then((data) => JSON.parse(data))
+  //     .then((json) => {
+  //       // Make a new array of all notes except the one with the ID provided in the URL
+  //       const result = json.filter((note) => note.id !== noteId);
 
-    // Save that array to the filesystem
-    writeToFile("./db/db.json", result);
+  //       // Save that array to the filesystem
+  //       writeToFile("./db/db.json", result);
 
-    // Respond to the DELETE request
-    res.json(`Item ${noteId} has been deleted.`);
-  });
+  //       // Respond to the DELETE request
+  //       res.json(`Item ${noteId} has been deleted.`);
+  //     });
 });
 
 // POST Route for a new note
