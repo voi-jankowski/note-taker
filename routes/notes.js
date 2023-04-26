e7090b3d8bba8cbbf00f522e348ac258a79dc77e
Voi <wojtek.wacek.dola@gmail.com>
2023-04-27 08:08:07 +1000

    Added comments to notes.js. and removed the console.logs.

--- routes/notes.js
+++ routes/notes.js
@@ -7,51 +7,42 @@ notes.get("/", async (req, res) => {
   let data = await fs.promises.readFile("./db/db.json", "utf-8");
   let parsedData = await res.json(JSON.parse(data));
   return parsedData;
-  //   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
 // DELETE Route for a specific note
 notes.delete("/:id", async (req, res) => {
+  // Create a variable with the id that has to be deleted
   const noteId = req.params.id;
-  console.log(noteId);
+  // Create an array with all the saved notes.
   let data = await fs.promises.readFile("./db/db.json", "utf-8");
   let parsedNotes = [].concat(JSON.parse(data));
   // Make a new array of all notes except the one with the ID provided in the URL
   let result = await parsedNotes.filter((note) => note.id !== noteId);
-  console.log("result in delete");
-  console.log(result);
+  // and save it to the db
   fs.writeFile("./db/db.json", JSON.stringify(result, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to db.json`)
   );
   res.json(`Item ${noteId} has been deleted.`);
-  //   readFromFile("./db/db.json")
-  //     .then((data) => JSON.parse(data))
-  //     .then((json) => {
-  //       // Make a new array of all notes except the one with the ID provided in the URL
-  //       const result = json.filter((note) => note.id !== noteId);
-
-  //       // Save that array to the filesystem
-  //       writeToFile("./db/db.json", result);
-
-  //       // Respond to the DELETE request
-  //       res.json(`Item ${noteId} has been deleted.`);
-  //     });
 });
 
 // POST Route for a new note
 notes.post("/", async (req, res) => {
   console.log(req.body);
+  //    Make the values in the object of the new note available
   const { title, text } = req.body;
+  //   And create new object with the unique id
   try {
     const newNote = {
       title,
       text,
       id: uuidv4(),
     };
-
+    // Create an array with all the saved notes.
     let data = await fs.promises.readFile("./db/db.json", "utf-8");
     let parsedData = JSON.parse(data);
+    // Push the new note into that array
     parsedData.push(newNote);
+    // and save it to the db
     fs.writeFile("./db/db.json", JSON.stringify(parsedData, null, 4), (err) =>
       err ? console.error(err) : console.info(`\nData written to db.json`)
     );
