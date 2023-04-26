9d6f99884bc70a0d9892d23fcdbc41da06e30d74
Voi <wojtek.wacek.dola@gmail.com>
2023-04-26 13:48:03 +1000

    Created index.js to route the apis

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,10 @@
 const express = require("express");
-const path = require("path");
-const api = require("./routes/index.js");
 
-const PORT = process.env.PORT || 3001;
+// Import our modular routers for /notes
+const notesRouter = require("./notes");
 
 const app = express();
 
-// Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/api", api);
+app.use("/notes", notesRouter);
 
-app.use(express.static("public"));
-
-// GET Route for homepage
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
-);
-
-// GET Route for notes page
-app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/notes.html"))
-);
-
-// Wildcard route to direct users to a 404 page
-// app.get("*", (req, res) =>
-//   res.sendFile(path.join(__dirname, "public/pages/404.html"))
-// );
-
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+module.exports = app;
